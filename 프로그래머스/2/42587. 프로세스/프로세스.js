6704a9d3abca6aa45132835e3bed7d5b3e0f8606
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-14 15:49:40 +0900

    [level 2] Title: 프로세스, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,25 @@
+function solution(priorities, location) {
+    let answer = 0;
+    const priorityMap = new Map();
+    for (let i = 0; i < priorities.length; i++) {
+        priorityMap.set(priorities[i], (priorityMap.get(priorities[i]) ?? 0) + 1);
+    }
+    let p = 9;
+    while (p > 0) {
+        if (!priorityMap.has(p)) {
+            p--;
+            continue;
+        }
+        for (let i = 0; i < priorities.length; i++) {
+            if (priorities[i] !== p) continue;
+            priorityMap.set(p, priorityMap.get(p) - 1);
+            answer++;
+            if (i === location) return answer;
+            if (priorityMap.get(p) === 0) {
+                p--;
+                while (!priorityMap.has(p)) p--;
+            }
+        }
+    }
+    return answer;
+}
