59dca0b9f00f8755287d3af28f2db433d3c29aa9
arbiterkdh <145187622+arbiterkdh@users.noreply.github.com>
2026-04-15 10:47:43 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -7,20 +7,18 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
-        br.close();
-        if (N == 0) {
-            bw.write("1");
-            bw.flush();
-            bw.close();
-            return;
+        long result = 1;
+
+        for (int i = 1; i <= N; i++) {
+            result *= i;
         }
-        long result = N;
-        while (N-- > 1) {
-            result *= N;
-        }
-        bw.write(String.valueOf(result));
+
+        bw.write(String.valueOf(result)); 
         bw.flush();
+
+        br.close();
         bw.close();
     }
 }
